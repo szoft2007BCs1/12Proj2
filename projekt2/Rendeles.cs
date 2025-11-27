d3cbbbe513584270cd046d507ba920b88415a77c
aArminHUN <132918931+aArminHUN@users.noreply.github.com>
2025-11-27 08:04:34 +0100

    alapja

---
+++ projekt2/Rendeles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt2
+{
+    class Rendeles
+    {
+        public string Nev {  get; set; }
+    }
+}
