6241325b34351555d410ed8b075cc2af364f7813
aArminHUN <132918931+aArminHUN@users.noreply.github.com>
2025-12-11 21:09:56 +0100

    A fájlhoz új sor hozzáadás gombbal

--- projekt2/MainWindow.xaml.cs
+++ projekt2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -30,33 +32,81 @@ namespace projekt2
             var maxOrder = Orders.OrderByDescending(x => x.TotalPrice).First();
             var byRestaurant = Orders.GroupBy(x => x.RestaurantName);
 
+            ComboBox_mindenes.Items.Clear();
+            ComboBox_mindenes.Items.Add("New");
             foreach (var item in Orders)
                 ComboBox_mindenes.Items.Add(item);
 
+            btn_add.Visibility = Visibility.Hidden;
         }
 
         private void ComboBox_mindenes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            txtblock_id.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].Id}";
-            txtblock_name.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].CustomerName}";
-            txtblock_phone.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].CustomerPhone}";
-            txtblock_street.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].Street}";
-            txtblock_restaurant.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].RestaurantName}";
-            txtblock_time.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].OrderDateTime}";
-            if (Orders[ComboBox_mindenes.SelectedIndex].Status == "New")
-                chckbox_status.IsChecked = true;
-            else
-                chckbox_status.IsChecked = false;
-
-            foreach (ComboBoxItem item in ComboBox_method.Items)
+            if (ComboBox_mindenes.SelectedItem is Order selectedOrder)
             {
-                if (item.Content.ToString() == Orders[ComboBox_mindenes.SelectedIndex].PaymentMethod)
+                btn_add.Visibility = Visibility.Hidden;
+
+                txtblock_id.Text = $"{selectedOrder.Id}";
+                txtblock_name.Text = $"{selectedOrder.CustomerName}";
+                txtblock_phone.Text = $"{selectedOrder.CustomerPhone}";
+                txtblock_street.Text = $"{selectedOrder.Street}";
+                txtblock_restaurant.Text = $"{selectedOrder.RestaurantName}";
+                txtblock_time.Text = $"{selectedOrder.OrderDateTime}";
+
+                if (selectedOrder.Status == "New")
+                    chckbox_status.IsChecked = true;
+                else
+                    chckbox_status.IsChecked = false;
+
+                foreach (ComboBoxItem item in ComboBox_method.Items)
                 {
-                    ComboBox_method.SelectedItem = item;
-                    break;
+                    if (item.Content.ToString() == selectedOrder.PaymentMethod)
+                    {
+                        ComboBox_method.SelectedItem = item;
+                        break;
+                    }
                 }
+                txtblock_price.Text = $"{selectedOrder.TotalPrice}";
             }
-            txtblock_price.Text = $"{Orders[ComboBox_mindenes.SelectedIndex].TotalPrice}";
+            else
+            {
+                btn_add.Visibility = Visibility.Visible;
+
+                txtblock_id.Text = $"{Orders.Count + 1}";
+                txtblock_name.Text = "Név";
+                txtblock_phone.Text = "Telefonszám";
+                txtblock_street.Text = "Utca";
+                txtblock_restaurant.Text = "Étterem";
+                txtblock_time.Text = "Idő";
+                chckbox_status.IsChecked = false;
+                ComboBox_method.SelectedIndex = 0;
+                txtblock_price.Text = "Ár";
+            }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            if(ComboBox_mindenes.SelectedItem == "New")
+            {
+                string tmp = "";
+                if (ComboBox_method.SelectedItem is ComboBoxItem selectedItem)
+                {
+                    tmp = selectedItem.Content.ToString();
+                }
+                string status = "";
+                if (chckbox_status.IsChecked == true)
+                    status = "New";
+                else
+                    status = "Delivered";
+                string fileszoveg = $"{txtblock_id.Text};{txtblock_name.Text};{txtblock_phone.Text};{txtblock_street.Text};{txtblock_restaurant.Text};{txtblock_time.Text};{status};{tmp};{txtblock_price.Text}";
+                File.AppendAllText("orders.txt", $"\n{fileszoveg}");
+
+                Orders = FileReader.Load("orders.txt");
+                ComboBox_mindenes.Items.Clear();
+                ComboBox_mindenes.Items.Add("New");
+                foreach (var item in Orders)
+                    ComboBox_mindenes.Items.Add(item);
+            }
         }
     }
 }
